e8ef27ddf2cb8fc815ea74edc9aa291e00afbd86
Konrad Wąsikiewicz <konrad.wasikiewicz@gmail.com>
2020-08-04 20:54:19 +0200

    Update Hangman.py

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -1,7 +1,7 @@
 import random   #word choice
 import string   #uppercase letters in english dict 
 
-from ThousandMostPopularWords import words  #imported list of 1000 most populr english nouns  
+from ThousandMostPopularWords import words  #imported list of 1000 most popular english nouns  
 
 
 def get_valid_word(words):   #excluding any words that might not be valid
@@ -10,6 +10,16 @@ def get_valid_word(words):   #excluding any words that might not be valid
         word = random.choice(words)
     return word.upper()
 
+def user_decision():     #exercised if guessing is complete
+    decision = input('\nWant to play again? (Y/N)').upper()
+    if decision == "Y":
+        hangman()
+    elif decision == "N":                
+        print('Thanks for playing!')
+    else:
+        print('You can only agree or disagree :)')
+        decision = input('\nWant to play again? (Y/N)').upper()
+
 def hangman():
     word = get_valid_word(words)
     word_letters = set(word) # keeps track of all the distinct letters in a word
@@ -17,8 +27,7 @@ def hangman():
     used_letters = set() # keeps track of what letter user have guessed (in alphabetical order)
     chances = 7
 
-    #user input
-    while len(word_letters) > 0 and chances > 0:
+    while len(word_letters) > 0 and chances > 0:  # len(word_letters) checks if you have already guessed it all 
         print('\nYou have', chances, 'chances and used these letters: ', ' '.join(used_letters))
 
         word_list = [letter if letter in used_letters else '_' for letter in word]
@@ -41,14 +50,15 @@ def hangman():
             print('\nYou have used an invalid character.')
     
     if chances == 0:
-        print('You died bro. The word was', word)
+        print('You died bro. The word was',word)
+        user_decision()
     else:
         print('Nice job bro. You got that. The word was ',word) 
+        user_decision()
 
 if __name__ == '__main__':
     hangman()
 
 
-# wanna try again option 
 #hangman rysunek 
 
