589438f1b07c7e879a0b7cd48fab4d630accb9a7
Konrad Wąsikiewicz <konrad.wasikiewicz@gmail.com>
2020-09-01 18:57:39 +0200

    Update Snake.py

--- Snake/Snake.py
+++ Snake/Snake.py
@@ -145,7 +145,13 @@ def randomFood(rows, items):
     while True:
         x = random.randrange(rows)
         y = random.randrange(rows)
-
+        if len(list(filter(lambda z:z.pos == (x,y), positions))) > 0:
+            continue
+        else:
+            break
+       
+    return (x,y)
+    
 def message_box(subject, content):
     pass
 
