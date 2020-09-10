099109f59df4103d9ba4284bd912ea99f23cefcc
Konrad Wąsikiewicz <konrad.wasikiewicz@gmail.com>
2020-09-10 20:07:33 +0200

    Update Snake.py

--- Snake/Snake.py
+++ Snake/Snake.py
@@ -155,6 +155,12 @@ def randomFood(rows, items):
 def message_box(subject, content):
     root = tk.Tk()
     root.attributes("-topmost", True)
+    root.withdraw()
+    messagebox.showinfo(subject, content)
+    try:
+        root.destroy()
+    except:
+        pass
 
 
 def main():
