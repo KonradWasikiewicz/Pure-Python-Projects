141f9a539f940ae86d72a97b25f9faaa3754af0a
Konrad Wąsikiewicz <konrad.wasikiewicz@gmail.com>
2020-09-14 19:49:03 +0200

    Update SortingAlgos.py

---
+++ Sorting Algos Visualized/SortingAlgos.py
@@ -0,0 +1,16 @@
+"""
+
+https://www.geeksforgeeks.org/analysis-of-different-sorting-techniques/
+https://www.tutorialspoint.com/python_data_structure/python_sorting_algorithms.htm
+
+
+Comparison based sorts:
+bubble, insertion, quick, selection, merge, heap
+
+Non-comparison based sorts:
+
+radix, count, bucket 
+
+"""
+
+
