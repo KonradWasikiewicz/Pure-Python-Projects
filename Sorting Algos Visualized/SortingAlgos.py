bf5a492b2826282edbdc44459facdfb054fd2935
Konrad Wąsikiewicz <konrad.wasikiewicz@gmail.com>
2020-09-14 19:55:27 +0200

    Update SortingAlgos.py

--- Sorting Algos Visualized/SortingAlgos.py
+++ Sorting Algos Visualized/SortingAlgos.py
@@ -2,6 +2,8 @@
 
 https://www.geeksforgeeks.org/analysis-of-different-sorting-techniques/
 https://www.tutorialspoint.com/python_data_structure/python_sorting_algorithms.htm
+https://medium.com/towards-artificial-intelligence/visualize-interesting-sorting-algorithms-with-python-bdd64bdd0713
+https://dev.to/kgprajwal/build-a-sorting-visualizer-in-python-2oej
 
 
 Comparison based sorts:
