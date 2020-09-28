0c1cdd29bda0d6ea60b0c38f85fb7cc6f95619ba
KonradWasikiewicz <konrad.wasikiewicz@gmail.com>
2020-09-28 12:09:07 +0200

    Update SortingAlgos.py

--- Sorting Algos Visualized/SortingAlgos.py
+++ Sorting Algos Visualized/SortingAlgos.py
@@ -16,3 +16,28 @@ radix, count, bucket
 """
 
 
+#miejsce na import i zmienne globalen
+
+class Algorithm:
+    def __init__(self, name):
+        self.array = random.sample(range(512), 512) # Random array of size 512
+        self.name = name # Get name of the variable
+
+    def update_display(self, swap1=None, swap2=None):                                                           # TO TRZEBA ZMIENIC 
+        import visualizer
+        visualizer.update(self, swap1, swap2) # pass the indexes to be swapped into the visualizer
+
+    def run(self): # Start the timer and run the algorithm
+        self.start_time = time.time() 
+        self.algorithm()
+        time_elapsed = time.time() - self.start_time
+        return self.array, time_elapsed
+
+
+
+
+'''Bubble sort - each pair of adjecent elements in a list is compared and elements are swapped if not in order'''
+
+class BubbleSort(Algorithm):
+    def __init__(self):
+        super().__init__("BubbleSort")
