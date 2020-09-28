8184a3b014d180f88c0448cc6972f10e13f0cf90
KonradWasikiewicz <konrad.wasikiewicz@gmail.com>
2020-09-28 18:34:53 +0200

    Update SortingAlgos.py

--- Sorting Algos Visualized/SortingAlgos.py
+++ Sorting Algos Visualized/SortingAlgos.py
@@ -4,7 +4,7 @@ https://www.geeksforgeeks.org/analysis-of-different-sorting-techniques/
 https://www.tutorialspoint.com/python_data_structure/python_sorting_algorithms.htm
 https://medium.com/towards-artificial-intelligence/visualize-interesting-sorting-algorithms-with-python-bdd64bdd0713
 https://dev.to/kgprajwal/build-a-sorting-visualizer-in-python-2oej
-
+https://www.educative.io/edpresso/merge-sort-in-python
 
 Comparison based sorts:
 bubble, insertion, quick, selection, merge, heap
