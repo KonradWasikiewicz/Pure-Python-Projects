ca5570b1f06b802b5bacab374b322dd11cc713cf
KonradWasikiewicz <konrad.wasikiewicz@gmail.com>
2020-09-29 13:42:56 +0200

    Update SortingAlgos.py

--- Sorting Algos Visualized/SortingAlgos.py
+++ Sorting Algos Visualized/SortingAlgos.py
@@ -55,7 +55,9 @@ class MergeSort(Algorithm):
     def __init__(self):
         super().__init__("MergeSort")
 
-    def algorithm(self):           
+    def algorithm(self, array = []):           
+        if array = []:
+            array = self.array
         if len(self.array) > 1:
             middle = len(self.array) // 2
             left = self.array[:middle]
@@ -92,3 +94,49 @@ class MergeSort(Algorithm):
                 j += 1
                 k += 1 
             self.update_display()
+ 
+''' Quick sort - picks an elements as a pivot and sorts the given array around the pivot'''
+class QuickSort(Algorithm):
+    def __init__(self):
+        super().__init__("QuickSort")
+
+    def algorithm(self, array = [], start=0, end=len(array) - 1):
+        if array = []:
+            array = self.array
+        if start >=  end:
+            return 
+        pivot = self.partition(array,start,end)
+        self.algorithm(array,start,pivot-1)
+        self.algorithm(array,pivot+1,end)
+
+    def partition(self, array, start, end):
+        pivot = array[start]
+        low = start+1
+        high = end
+    
+        while True:
+            # If the current value we're looking at is larger than the pivot
+            # it's in the right place (right side of pivot) and we can move left,
+            # to the next element.
+            # We also need to make sure we haven't surpassed the low pointer, since that
+            # indicates we have already moved all the elements to their correct side of the pivot
+            while low <= high and array[high] >= pivot:
+                high = high - 1
+    
+            # Opposite process of the one above
+            while low <= high and array[low] <= pivot:
+                low = low + 1
+    
+            # We either found a value for both high and low that is out of order
+            # or low is higher than high, in which case we exit the loop
+            if low <= high:
+                array[low], array[high] = array[high], array[low]
+                # The loop continues
+            else:
+                # We exit out of the loop
+                break
+    
+        array[start], array[high] = array[high], array[start]
+        self.update_display(array[start], array[high])
+        
+        return high
