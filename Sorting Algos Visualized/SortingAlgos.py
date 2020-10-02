3b8c7432ff73ea6e163c9f31ebe360e08ac8823a
Konrad Wąsikiewicz <konrad.wasikiewicz@gmail.com>
2020-10-02 19:45:27 +0200

    Update SortingAlgos.py

--- Sorting Algos Visualized/SortingAlgos.py
+++ Sorting Algos Visualized/SortingAlgos.py
@@ -222,33 +222,47 @@ def heap_sort(self):
     
     last_pos_pointer -= 1
 
-    
-    def heapify(arr, n, i): 
-    largest = i
-    l = 2 * i + 1
-    r = 2 * i + 2
-
-    if l < n and arr[i] < arr[l]:
-      largest = l
-    
-    if r < n and arr[largest] < arr[r]:
-      largest = r
 
 
-    if largest != i:
-      arr[i], arr[largest] = arr[largest], arr[i]
-      heapify(arr, n, largest)
 
-def heapSort(arr):
-  n = len(arr)
-
-  for i in range(n //2, -1, -1):
-    heapify(arr, n, i)
-
-  for i in range(n - 1, 0, -1): 
-    arr[i], arr[0] = arr[0], arr[i]
-    heapify(arr, i, 0)
-
-arr = [11, 34, 9, 5, 16, 10] 
-
-print(heapSort(arr)) 
+# To heapify subtree rooted at index i. 
+# n is size of heap 
+def heapify(arr, lenght, i): 
+    largest = i  # Initialize largest as root 
+    left = 2 * i + 1     # left = 2*i + 1 
+    right = 2 * i + 2     # right = 2*i + 2 
+  
+    # See if left child of root exists and is 
+    # greater than root 
+    if left < lenght and arr[i] < arr[left]: 
+        largest = left 
+  
+    # See if right child of root exists and is 
+    # greater than root 
+    if right < lenght and arr[largest] < arr[right]: 
+        largest = right 
+  
+    # Change root, if needed 
+    if largest != i: 
+        arr[i],arr[largest] = arr[largest],arr[i]  # swap 
+  
+        # Heapify the root. 
+        heapify(arr, n, largest) 
+  
+# The main function to sort an array of given size 
+def heapSort(arr): 
+    n = len(arr) 
+  
+    # Build a maxheap. 
+    # Since last parent will be at ((n//2)-1) we can start at that location. 
+    for i in range(n // 2 - 1, -1, -1): 
+        heapify(arr, n, i) 
+  
+    # One by one extract elements 
+    for i in range(n-1, 0, -1): 
+        arr[i], arr[0] = arr[0], arr[i]   # swap 
+        heapify(arr, i, 0) 
+  
+# Driver code to test above 
+arr = [ 12, 11, 13, 5, 6, 7] 
+heapSort(arr) 
